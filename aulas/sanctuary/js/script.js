72b9edf7c17c802ebcafac0fc5378ad89dd67325
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2025-02-19 22:16:22 -0300

    a

---
+++ aulas/sanctuary/js/script.js
@@ -0,0 +1,38 @@
+// #region video
+window.addEventListener("load", () => {
+    let video = window.document.querySelector("#video");
+
+    video.play();
+    video.loop = true;
+    video.muted = true;
+
+});
+//#endregion video
+
+//#region temas
+
+const btnLight = document.querySelector("#lightMode");
+const btnDark = document.querySelector("#darkMode");
+const body = document.body;
+
+body.classList.add("lightMode");
+btnLight.style.display = "inline";
+btnDark.style.display = "none";
+
+btnLight.addEventListener("click", () => {
+    body.classList.remove("lightMode");
+    btnLight.style.display = "none";
+
+    body.classList.add("darkMode");
+    btnDark.style.display = "inline";
+});
+
+btnDark.addEventListener("click", () => {
+    body.classList.remove("darkMode");
+    btnDark.style.display = "none";
+
+    body.classList.add("lightMode");
+    btnLight.style.display = "inline";
+});
+
+//#region temas
