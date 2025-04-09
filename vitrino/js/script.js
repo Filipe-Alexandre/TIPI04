d0a14311e93c0441e9817a43a11bf71c867dbe02
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2025-04-08 22:14:16 -0300

    portugol
    Começando aulas de lógica com portugol

--- vitrino/js/script.js
+++ vitrino/js/script.js
@@ -8,7 +8,6 @@ const missao = document.querySelector(".missaoDiv");
 const visao = document.querySelector(".visaoDiv");
 const valores = document.querySelector(".valoresDiv");
 
-
 btnSobre.addEventListener("click", ()=> {
     sobre.style.display = "flex";
     missao.style.display = "none";
@@ -45,12 +44,35 @@ const body = document.body
 darkBtn.addEventListener("click", ()=> {
     body.classList.remove("lightMode");
     body.classList.add("darkMode");
-
-})
-
+    lightBtn.style.display="flex";
+    darkBtn.style.display="none";
+}) 
 
 lightBtn.addEventListener("click", ()=> {
     body.classList.remove("darkMode");
     body.classList.add("lightMode");
-
+    darkBtn.style.display="flex";
+    lightBtn.style.display="none";
 })
+
+// +100px para os links
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("a[href^='#']").forEach(anchor => {
+        anchor.addEventListener("click", function (event) {
+            event.preventDefault();
+            
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+            
+            if (targetElement) {
+                const offset = 100;
+                const elementPosition = targetElement.getBoundingClientRect().top + window.scrollY;
+                
+                window.scrollTo({
+                    top: elementPosition - offset,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
