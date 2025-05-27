04ac5dc90d0f8e9d8cbce90ee61877c71cf1fb5a
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2025-05-26 21:48:41 -0300

    a

--- aulas/saida-02-Back-end/CSharp/10-tabuada/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿Console.WriteLine("informe um numero para exibir a tabuada");
-int num = int.Parse(Console.ReadLine());
-
-for (int i = 0; i <= 10; i++)
-{
-    Console.WriteLine($"{num} x {i} = {num*i}");
-}
